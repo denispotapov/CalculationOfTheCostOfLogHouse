53f2218a481da51d9f2aaeadb608b630a6485536
denispotapov <fantom.r45@mail.ru>
2021-05-03 16:18:57 +0300

    Edited the price of a section of a log house

--- app/src/main/java/com/denispotapov/calculationofthecostofloghouse/util/constants.kt
+++ app/src/main/java/com/denispotapov/calculationofthecostofloghouse/util/constants.kt
@@ -6,9 +6,9 @@ const val SECTION_120 = 0.12
 const val SECTION_140 = 0.14
 
 const val PRICE_SECTION_80 = 280
-const val PRICE_SECTION_100 = 404
-const val PRICE_SECTION_120 = 582
-const val PRICE_SECTION_140 = 790
+const val PRICE_SECTION_100 = 430
+const val PRICE_SECTION_120 = 620
+const val PRICE_SECTION_140 = 840
 
 const val WEIGHT_SECTION_80 = 18
 const val WEIGHT_SECTION_100 = 28
